23128425a14c819b3d241e3c34f2d310c9444e51
MariferVL <mfvl.1186@gmail.com>
2025-07-29 13:54:38 -0400

    Fix manifest error

--- middleware.js
+++ middleware.js
@@ -1,16 +1,16 @@
-import { NextResponse } from 'next/server';
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
+import { NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
 
-let locales = ['es', 'en'];
-export let defaultLocale = 'es';
+let locales = ["es", "en"];
+export let defaultLocale = "es";
 
 function getLocale(request) {
   const negotiatorHeaders = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  
+
   return match(languages, locales, defaultLocale);
 }
 
@@ -25,17 +25,18 @@ export function middleware(request) {
 
   const locale = getLocale(request);
   request.nextUrl.pathname = `/${locale}${pathname}`;
-  
+
   return NextResponse.redirect(request.nextUrl);
 }
 
 export const config = {
   matcher: [
     {
-      source:     '/((?!api|_next/static|_next/image|images|videos|favicon.ico).*)',     
+      source:
+        "/((?!api|_next/static|_next/image|images|videos|favicon.ico|site.webmanifest).*)",
       missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
       ],
     },
   ],
